664b05db5a8f57318e6eb980d34bb03fdb0203eb
natalieybl <94332856+natalieybl@users.noreply.github.com>
2024-08-29 20:00:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template, Request
+from flask import Flask,render_template,request
 
 app=Flask(__name__)
 
-@app.route('/', methods=["get","post"])
+@app.route('/',methods=["get","post"])
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
-  if __name__=='__main__':
+@app.route('/store_money',methods=["get","post"])
+def store_money():
+    return render_template('store_money.html')
+
+@app.route('/transfer_money',methods=["get","post"])
+def transfer_money():
+    return render_template('transfer_money.html')
+
+if __name__=='__main__':
     app.run()
