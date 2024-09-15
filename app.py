5871b0c11d86b7b2631843ce054d38819179d263
natalieybl <94332856+natalieybl@users.noreply.github.com>
2024-09-15 14:04:58 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -11,14 +11,19 @@ def index():
 @app.route('/main',methods=["get","post"])
 def main():
     r = request.form.get("q")
-    current_time = datetime.datetime.now()
-    conn = sqlite3.connect('dapp.db')
-    c = conn.cursor()
-    c.execute('insert into user values(?,?)', (r, current_time))
-    conn.commit()
-    c.close()
-    conn.close()
-
+    #print('back from view.html')
+    #print(r)
+    if r == None: 
+        pass
+    else:
+        current_time = datetime.datetime.now()
+        conn = sqlite3.connect('dapp.db')
+        c = conn.cursor()
+        c.execute('insert into user values(?,?)', (r, current_time))
+        conn.commit()
+        c.close()
+        conn.close()
+    
     return render_template('main.html', r = r)
 
 @app.route('/store_money',methods=["get","post"])
@@ -41,6 +46,43 @@ def admin():
 def viewDB():
     conn = sqlite3.connect('dapp.db')
     c = conn.cursor()
+    # Execute a SELECT statement to retrieve all records from the table
+    c.execute('SELECT * FROM user')
+
+    # Fetch all the records
+    rows = c.fetchall()
+
+    # Get the column names
+    column_names = [description[0] for description in c.description]
+    r = ""
+    # Create the HTML table
+    html_table = '''
+    <table border="1" cellpadding="5" cellspacing="0">
+        <tr>
+            {}
+        </tr>
+        {}
+    </table>
+    '''
+
+    # Create the table header row
+    table_header_row = '<th>{}</th>'.format('</th><th>'.join(column_names))
+
+    # Create the table rows
+    table_rows = ''
+    for row in rows:
+        table_row = '<td>{}</td>'.format('</td><td>'.join(str(value) for value in row))
+        table_rows += '<tr>{}</tr>'.format(table_row)
+
+    # Assemble the HTML table
+    html_table = html_table.format(table_header_row, table_rows)
+
+    # Print the HTML table
+    print(html_table)
+
+    # Close the connection
+    conn.close()
+    '''
     c.execute('select * from user')
     r = ""
     for row in c.fetchall():
@@ -49,7 +91,8 @@ def viewDB():
     conn.commit()
     c.close()
     conn.close()
-    return render_template('view.html', r = r)
+    '''
+    return render_template('view.html', r = r, html_table = html_table)
 
 @app.route('/delDB',methods=["get","post"])
 def delDB():
